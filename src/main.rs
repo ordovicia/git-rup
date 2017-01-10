1822b3d85728b6486576bb753f1630598522d6ed
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-01-11 02:23:21 +0900

    Impl fetch operation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate git2;
+use git2::{Repository, FetchOptions, FetchPrune, AutotagOption};
 
 const PROGNAME: &'static str = "git-rup";
 
@@ -10,6 +11,10 @@ fn main() {
         }};
     }
 
+    macro_rules! warn {
+        ($($args: expr),*) => { info!($($args),*); };
+    }
+
     macro_rules! fail {
         ($($args: expr),*) => {{
             print!("[{}] ", PROGNAME);
@@ -18,7 +23,7 @@ fn main() {
         }};
     }
 
-    let repo = match git2::Repository::open(".") {
+    let repo = match Repository::open(".") {
         Ok(repo) => repo,
         Err(e) => fail!("{}", e),
     };
@@ -38,5 +43,28 @@ fn main() {
                 info!("    none UTF-8 remote name");
             }
         }
+    }
+    info!();
+
+    for r in remotes.iter() {
+        if r.is_none() {
+            continue;
+        }
+        let r = r.unwrap();
+
+        info!("fetching from {}...", r);
+
+        let mut fetch_options = FetchOptions::new();
+        fetch_options.prune(FetchPrune::On).download_tags(AutotagOption::All);
+
+        match repo.find_remote(r)
+            .and_then(|ref mut remote| remote.fetch(&[], Some(&mut fetch_options), None)) {
+            Ok(_) => {
+                info!("fetched successfully");
+            }
+            Err(e) => {
+                warn!("{}", e);
+            }
+        };
     }
 }
